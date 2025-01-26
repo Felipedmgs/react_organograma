9a0789547b33f9f358ff2b1b90051518ac4813da
Felipedmgs <felipedd@hotmail.com>
2025-01-26 20:39:09 -0300

    first commit

---
+++ src/componentes/Banner/index.js
@@ -0,0 +1,16 @@
+import './Banner.css'
+
+function Banner(){
+    return(
+        <>        
+            <header className='banner'>
+            <img src="/imagens/banner.png" alt="banner da imagem"/>
+            </header>
+            <h1>Organograma</h1>
+        </>
+    )
+
+
+}
+
+export default Banner
